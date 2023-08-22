342139810777ddf5e0530d4d19dbb372452363e2
boris920308 <jhoonj0308@gmail.com>
2023-08-22 11:01:09 +0900

    [refactor] LiveData -> SharedFlow

--- HoonSearch/app/src/main/java/hoon/example/hoonsearch/HomeFragment.kt
+++ HoonSearch/app/src/main/java/hoon/example/hoonsearch/HomeFragment.kt
@@ -7,8 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import hoon.example.hoonsearch.databinding.FragmentHomeBinding
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment(){
     private var _binding: FragmentHomeBinding? = null
@@ -31,9 +32,11 @@ class HomeFragment : Fragment(){
             viewModel.getSearchResult("book", "kotlin")
         }
 
-        viewModel.searchResult.observe(viewLifecycleOwner, Observer {
-            Log.d("hoon92", "resultList = ${it.toString()}")
-        })
+        lifecycleScope.launch {
+            viewModel.searchFlow.collect {
+                Log.d("hoon92", "searchResult = ${it.toString()}")
+            }
+        }
     }
 
 }
--- HoonSearch/app/src/main/java/hoon/example/hoonsearch/HomeViewModel.kt
+++ HoonSearch/app/src/main/java/hoon/example/hoonsearch/HomeViewModel.kt
@@ -1,22 +1,23 @@
 package hoon.example.hoonsearch
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import hoon.example.hoonsearch.network.NaverAPI
 import hoon.example.hoonsearch.network.NaverSearchResponse
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
-    private val _searchResult = MutableLiveData<NaverSearchResponse>()
-    val searchResult: LiveData<NaverSearchResponse> = _searchResult
+
+    private val _searchFlow = MutableSharedFlow<NaverSearchResponse>()
+    val searchFlow = _searchFlow.asSharedFlow()
 
     fun getSearchResult(key: String, searchKeyword: String) {
         viewModelScope.launch {
             try {
-                _searchResult.postValue(NaverAPI.naverApiService.getSearch(key, searchKeyword, 20, null))
+                _searchFlow.emit(NaverAPI.naverApiService.getSearch(key, searchKeyword, 20, null))
             } catch (e: Exception) {
                 Log.e("hoon92", "err = $e")
             }
